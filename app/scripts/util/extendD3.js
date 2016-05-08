0e5e4f68d89cb0a2a3c53dbf451f56a254ef0724
Lian <lianliu19910622@gmail.com>
2016-05-08 13:32:48 -0500

    beautify bus markers; code clean up

--- app/scripts/util/extendD3.js
+++ app/scripts/util/extendD3.js
@@ -9,33 +9,42 @@ d3.selection.prototype.drawVehicle = function(busData, projection) {
 		ANGLE: d.$.heading
 	 */
 	buses.enter()
-		.append( 'circle' )
-		.attr("r", "4px")
-		.attr("fill", "black");
+		.append('svg:path')
+		.attr('d', 'M0,0l-8.8-17.7C-12.1-24.3-7.4-32,0-32h0c7.4,0,12.1,7.7,8.8,14.3L0,0z')
+		//.attr("r", "4px")
+		.attr('fill', '#31558d')
+		.attr('fill-opacity', '0.75');
 
 	buses.exit()
 		.remove();
 
 
-	buses.attr("class", 'bus')
-		.attr("data-route-tag", function (d) { 
+	buses.attr('class', 'bus')
+		.attr('data-route-tag', function (d) { 
 			return d.$.routeTag; 
 		})
-		.attr("data-dir-tag", function (d) { 
+		.attr('data-dir-tag', function (d) { 
 			return d.$.dirTag; 
 		})
-		.attr("data-heading", function (d) { 
+		.attr('data-heading', function (d) { 
 			return d.$.heading; 
 		})
-		.attr("data-id", function (d) { 
+		.attr('data-id', function (d) { 
 			return d.$.id; 
 		})
+		.attr("transform", function(d) {
+			return "translate(" 
+				+ projection([d.$.lon,d.$.lat])[0] + "," 
+				+ projection([d.$.lon,d.$.lat])[1] + ") scale(.75)";
+		})
+		/*
 		.attr("cx", function (d) { 
 			return projection([d.$.lon,d.$.lat])[0]; 
 		})
 		.attr("cy", function (d) { 
 			return projection([d.$.lon,d.$.lat])[1]; 
 		})
+		*/
 
 	return buses;
 };
